ee5e920a0b6feeede8b07bcdab363ca034131dba
DouglasFuelber <dffuelber@gmail.com>
2019-11-06 22:31:15 -0300

    Added models and mongodb to project

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Start app
 const app = express();
 
+// Start DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Route definition
 app.get('/', (req, res) => {
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebok/react-native"
+    });
+
     res.send('Hello Douglas');
 });
 
